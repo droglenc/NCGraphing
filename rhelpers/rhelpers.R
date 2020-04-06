bf57d979bffa9ba63933667df0360ce90c943f41
Derek Ogle <droglenc@users.noreply.github.com>
2020-04-06 10:15:04 -0500

    Update rhelpers.R

--- rhelpers/rhelpers.R
+++ rhelpers/rhelpers.R
@@ -1,17 +1,14 @@
-##############################################################
-# Creates (if need2render=TRUE) and modifies the created HTML
-#   file to better utilize GitHub's abilities.
-# Modifications are ...
-# 1. Remove a bunch of header stuff from the HTML file and
-#    replace it with the YAML header in the Rmd file.
-# 2. Remove the hard-wired RStudio ordered lists so that the
-#    the first level will be numbers (the GitHub) default and
-#    the second level will be letters (as per the CSS line
-#    at the bottom of each Rmd file).
-# Note that for some reason, some Rmd files need to be
-#   rendered with the Knit HTML button in RStudio.  In these
-#   cases, do that and then use need2render=FALSE in this
-#   function.
+################################################################################
+# Creates (if need2render=TRUE) and modifies the created HTML file to better
+# utilize GitHub's abilities. Modifications are ...
+# 1. Remove a bunch of header stuff from the HTML file and replace it with the
+#    YAML header in the Rmd file.
+# 2. Remove the hard-wired RStudio ordered lists so that the the first level
+#    will be numbers (the GitHub) default and the second level will be letters
+#    (as per the CSS line at the bottom of each Rmd file).
+# Note that for some reason, some Rmd files need to be rendered with the Knit
+# HTML button in RStudio. In these cases, do that and then use need2render=FALSE
+# in this function.
 ##############################################################
 modHTML <- function(f,need2render=TRUE) {
   # Render the HTML file
@@ -19,14 +16,20 @@ modHTML <- function(f,need2render=TRUE) {
     require(rmarkdown)
     render(paste0(f,".Rmd"),output_format="all",clean=FALSE)
   }
+  # Get directory and file names
+  d <- dirname(f)
+  fn <- basename(f)
   # Delete .md files left over because clean=FALSE was needed
-  tmp <- list.files(pattern="\\.md")
-  if (any(grepl(f,tmp))) file.remove(tmp[which(grepl(f,tmp))])
+  tmp <- list.files(path=d,pattern="\\.md")
+  if (any(grepl(fn,tmp))) 
+    file.remove(file.path(d,tmp[which(grepl(fn,tmp))]))
   # Delete files in directories in f_files that are not needed on the webpage
-  unlink(paste0(getwd(),"/",f,"_files","/bootstrap-3.3.5"),recursive=TRUE)
-  unlink(paste0(getwd(),"/",f,"_files","/jquery-1.11.3"),recursive=TRUE)
-  unlink(paste0(getwd(),"/",f,"_files","/highlight"),recursive=TRUE)
-  unlink(paste0(getwd(),"/",f,"_files","/navigation-1.0"),recursive=TRUE)
+  dn2 <- file.path(d,paste0(fn,"_files"))
+  unlink(file.path(dn2,"bootstrap-3.3.5"),recursive=TRUE)
+  unlink(file.path(dn2,"jquery-1.11.3"),recursive=TRUE)
+  unlink(file.path(dn2,"highlight"),recursive=TRUE)
+  unlink(file.path(dn2,"navigation-1.0"),recursive=TRUE)
+
   # Read in HTML and RMarkdown files
   h <- readLines(paste0(f,".html"))
   r <- readLines(paste0(f,".Rmd"))
@@ -44,7 +47,10 @@ modHTML <- function(f,need2render=TRUE) {
     if (tmp1[1]<tmp2) h <- h[-(1:tmp1[1])]
   }
   # Get the layout, title, subtitle, author, and css from RMD file
-  tmp <- c(which(grepl("layout:",r)),which(grepl("title:",r)),which(grepl("author:",r)),which(grepl("css:",r)))
+  tmp <- c(which(grepl("layout:",r)),
+           which(grepl("title:",r)),
+           which(grepl("author:",r)),
+           which(grepl("css:",r)))
   r <- r[tmp]
   # Put it all together as a new html file
   h <- c("---",r,"---",h)
