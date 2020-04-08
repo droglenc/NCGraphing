f38e9594571a34b56b3f50e896c6dbcd78b0b0d2
Derek Ogle <droglenc@users.noreply.github.com>
2020-04-08 15:53:46 -0500

    Updated Black Bears Lecture

--- modules/Intro/Lecture_MooseWolves.R
+++ modules/Intro/Lecture_MooseWolves.R
@@ -12,98 +12,67 @@ irmw %<>%
          ice_bridges=factor(ice_bridges))
 head(irmw)
 
-head(irmw)
-
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves))
-
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_point()
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_line()
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_bar(stat="identity")
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_point() +
   geom_line() +
   geom_rug()
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_point(color="darkred",size=3) +
   geom_line(size=1.5,linetype="dashed")
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_line(size=1.5,linetype="dashed") +
   geom_point(color="darkred",size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves,
-                        color="darkred")) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves,color="darkred")) +
   geom_line() +
   geom_point(size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves,
-                        color=ice_bridges)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves,color=ice_bridges)) +
   geom_line() +
   geom_point(size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
-  geom_line() + #BREAK
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
+  geom_line() +
   geom_point(aes(color=ice_bridges),size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
-  geom_line() + #BREAK
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
+  geom_line() +
   geom_point(aes(color=winter_temp),size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
-  geom_line() + #BREAK
-  geom_point(aes(color=winter_temp,
-                 shape=ice_bridges),
-             size=3)
-
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_line() +
-  geom_point(aes(color=ice_bridges),size=3) + #BREAK
-  scale_color_manual(values=c("blue","red"),
-                     name="Ice Bridge?") + #BREAK
-  scale_x_continuous(name="Year",
-                     breaks=seq(1960,2020,5),
-                     minor_breaks=seq(1959,2019,1)) + #BREAK
-  scale_y_continuous(name="Estimated Number of Wolves",
-                     breaks=seq(0,50,5),
-                     minor_breaks=NULL)
+  geom_point(aes(color=winter_temp,shape=ice_bridges),size=3)
 
-p <- ggplot(data=irmw,
-            mapping=aes(x=year,y=wolves)) +
+p <- ggplot(data=irmw,mapping=aes(x=year,y=wolves)) +
   geom_line() +
   geom_point(aes(color=ice_bridges),size=3) +
-  scale_color_manual(values=c("blue","red"),
-                     name="Ice Bridge?") +
+  scale_color_manual(values=c("blue","red"),name="Ice Bridge?") +
   scale_x_continuous(name="Year",
-                     breaks=seq(1960,2020,5)) +
+                     breaks=seq(1960,2020,5),minor_breaks=seq(1959,2019,1)) +
   scale_y_continuous(name="Estimated Number of Wolves",
-                     breaks=seq(0,50,5)) + #BREAK
+                     breaks=seq(0,50,5),minor_breaks=NULL)
+
+p <- ggplot(data=irmw, mapping=aes(x=year,y=wolves)) +
+  geom_line() +
+  geom_point(aes(color=ice_bridges),size=3) +
+  scale_color_manual(values=c("blue","red"),name="Ice Bridge?") +
+  scale_x_continuous(name="Year",breaks=seq(1960,2020,5)) +
+  scale_y_continuous(name="Estimated Number of Wolves",breaks=seq(0,50,5)) +
   theme_classic()
 
- p + #BREAK
+p +
   theme_minimal()
 
-p + #BREAK
+p +
   theme_bw()
-
-
-# Script created at 2020-04-08 15:38:25
