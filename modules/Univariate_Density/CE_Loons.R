7b04ca89232cbc0d83f0f359348552d3a05f1092
Derek Ogle <droglenc@users.noreply.github.com>
2020-04-10 13:18:14 -0500

    again

--- modules/Univariate_Density/CE_Loons.R
+++ modules/Univariate_Density/CE_Loons.R
@@ -1,5 +1,7 @@
 # Renders an appropriate HTML file for the webpage (CTRL-S and CTRL-ALT-C)
 
+library(knitr)
+
 loon <- read.csv("https://raw.githubusercontent.com/droglenc/NCData/master/Loon1.csv")
 str(loon)
 
@@ -80,4 +82,4 @@ wt7 <- ggplot(data=loon,mapping=aes(y=sex,x=weight,color=sex,fill=sex)) +
 wt7
 
 
-# Script created at 2020-04-10 13:11:33
+# Script created at 2020-04-10 13:17:50
