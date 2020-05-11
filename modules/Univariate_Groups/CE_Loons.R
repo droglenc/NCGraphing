72a842b1a44d8d11460fff32f9c9c0876638f860
Derek Ogle <droglenc@users.noreply.github.com>
2020-05-11 18:33:26 -0500

    Updated CE Keys

---
+++ modules/Univariate_Groups/CE_Loons.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+#!# Set to your own working directory and have just your filename below.
+loon <- read.csv("https://raw.githubusercontent.com/droglenc/NCData/master/Loon1.csv")
+
+reg1 <- ggplot(data=loon,mapping=aes(x=region)) +
+  geom_bar(color="gray30",fill="wheat") +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Frequency of Loons",expand=expansion(mult=c(0,0.05))) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+reg1
+
+regsex1 <- ggplot(data=loon,mapping=aes(x=region,fill=sex)) +
+  geom_bar(color="gray30",alpha=0.7) +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Frequency of Loons",expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Sex",values=c("#E69F00","#0072B2","#CC79A7")) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+regsex1
+
+regsex2 <- ggplot(data=loon,mapping=aes(x=region,fill=sex)) +
+  geom_bar(position="dodge",color="gray30",alpha=0.7) +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Frequency of Loons",expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Sex",values=c("#E69F00","#0072B2","#CC79A7")) +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+regsex2
+
+sum1 <- loon %>%
+  group_by(region) %>%
+  summarize(freq=n()) %>%
+  ungroup()
+
+reg1a <- ggplot(data=sum1,mapping=aes(x=region,y=freq)) +
+  geom_bar(stat="identity",color="gray30",fill="wheat") +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Frequency of Loons",expand=expansion(mult=c(0,0.05))) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+reg1a
+
+sum2 <- loon %>%
+  group_by(region,sex) %>%
+  summarize(freq=n()) %>%
+  mutate(perc=freq/sum(freq)*100) %>%
+  ungroup()
+  
+regsex3 <- ggplot(data=sum2,mapping=aes(x=region,y=freq,fill=sex)) +
+  geom_bar(stat="identity",color="gray30",alpha=0.7) +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Frequency of Loons",expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Sex",values=c("#E69F00","#0072B2","#CC79A7")) +
+  coord_flip() +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+regsex3
+
+regsex4 <- ggplot(data=sum2,mapping=aes(x=region,y=perc,fill=sex)) +
+  geom_bar(stat="identity",color="gray30",alpha=0.7) +
+  scale_x_discrete(name="Region of Capture") +
+  scale_y_continuous(name="Percentage of Loons",expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Sex",values=c("#E69F00","#0072B2","#CC79A7")) +
+  coord_flip() +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+regsex4
+
+regsex5 <- ggplot(data=sum2,mapping=aes(x=sex,y=region,fill=perc)) +
+  geom_tile() +
+  scale_fill_gradient2(low="orange1",high="orange4") +
+  geom_text(aes(label=paste0(round(perc,0),"%"))) +
+  scale_y_discrete(name=NULL,expand=expansion(mult=0)) +
+  scale_x_discrete(name=NULL,expand=expansion(mult=0)) +
+  theme_minimal() +
+  theme(legend.position="none",
+        aspect.ratio=1,
+        panel.grid=element_blank()) +
+  labs(title="Sex Distribution of Loons",
+       subtitle="by region of North America")
+regsex5
+
+
+# Script created at 2020-05-11 18:33:09
---
+++ modules/Univariate_Groups/CE_Wolves.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+lvls_agree <- c("Strongly Disagree","Disagree","Neutral","Agree","Strongly Agree")
+
+#!# Set to your own working directory and have just your filename below.
+sow <- read.csv("https://raw.githubusercontent.com/droglenc/NCData/master/StateOfWolf.csv") %>%
+  select(state,hunt_deer,seen_wolf,part_deer,
+         ad_healthy_deer,ad_nowolves_deer,ad_threaten_deer,ad_deer_hunting) %>%
+  filter(complete.cases(.)) %>%
+  mutate(part_deer=factor(part_deer,
+          levels=c("Stop Participating","Participate Less Frequently",
+                   "Participate More Frequently","No Changes")),
+         ad_healthy_deer=factor(ad_healthy_deer,levels=lvls_agree),
+         ad_nowolves_deer=factor(ad_nowolves_deer,levels=lvls_agree),
+         ad_threaten_deer=factor(ad_threaten_deer,levels=lvls_agree),
+         ad_deer_hunting=factor(ad_deer_hunting,levels=lvls_agree)) %>%
+  filter(hunt_deer=="Yes")
+
+ngdh1 <- ggplot(data=sow,mapping=aes(x=ad_deer_hunting)) +
+  geom_bar(color="gray30",fill="wheat") +
+  scale_x_discrete(name="Level of Agreement") +
+  scale_y_continuous(name="Frequency of Respondents",
+                     expand=expansion(mult=c(0,0.05))) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+ngdh1
+
+NGDHst1 <- ggplot(data=sow,mapping=aes(x=state,fill=ad_deer_hunting)) +
+  geom_bar(color="gray30",alpha=0.7) +
+  scale_x_discrete(name="State") +
+  scale_y_continuous(name="Frequency of Respondents",
+                     expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Agreement",
+                    values=c("#E69F00","#56B4E9","#009E73","#F0E442","#0072B2")) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+NGDHst1
+
+NGDHst2 <- ggplot(data=sow,mapping=aes(x=state,fill=ad_deer_hunting)) +
+  geom_bar(color="gray30",alpha=0.7) +
+  scale_x_discrete(name="State") +
+  scale_y_continuous(name="Frequency of Respondents",
+                     expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Agreement",
+                    values=c("#E69F00","#56B4E9","#009E73","#F0E442","#0072B2")) +
+  coord_flip() +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+NGDHst2
+
+sum1 <- sow %>%
+  group_by(ad_deer_hunting) %>%
+  summarize(freq=n()) %>%
+  ungroup()
+
+ngdh2 <- ggplot(data=sum1,mapping=aes(x=ad_deer_hunting,y=freq)) +
+  geom_bar(stat="identity",color="gray30",fill="wheat") +
+  scale_x_discrete(name="Level of Agreement") +
+  scale_y_continuous(name="Frequency of Respondents",
+                     expand=expansion(mult=c(0,0.05))) +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+ngdh2
+
+sum2 <- sow %>%
+  group_by(state,ad_deer_hunting) %>%
+  summarize(freq=n()) %>%
+  mutate(perc=freq/sum(freq)*100) %>%
+  ungroup()
+  
+NGDHst3 <- ggplot(data=sum2,mapping=aes(x=state,y=freq,fill=ad_deer_hunting)) +
+  geom_bar(stat="identity",color="gray30",alpha=0.7) +
+  scale_x_discrete(name="State") +
+  scale_y_continuous(name="Frequency of Respondents",
+                     expand=expansion(mult=c(0,0.05))) +
+  scale_fill_manual(name="Agreement",
+                    values=c("#E69F00","#56B4E9","#009E73","#F0E442","#0072B2")) +
+  coord_flip() +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+NGDHst3
+
+NGDHst4 <- ggplot(data=sum2,mapping=aes(x=state,y=perc,fill=ad_deer_hunting)) +
+  geom_bar(stat="identity",color="gray30",alpha=0.7) +
+  scale_x_discrete(name="State") +
+  scale_y_continuous(name="Percent of Respondents",
+                     expand=expansion(mult=0)) +
+  scale_fill_manual(name="Agreement",
+                    values=c("#E69F00","#56B4E9","#009E73","#F0E442","#0072B2")) +
+  coord_flip() +
+  theme_bw() +  
+  theme(panel.grid.major.x=element_blank(),panel.grid.minor.x=element_blank())
+NGDHst4
+
+
+# Script created at 2020-05-11 18:27:49
